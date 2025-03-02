c67b9fa9b79ea1466a5eccf7d19bda7631029367
Alexey <codenamed.asyncz@gmail.com>
2025-03-02 16:19:35 +0300

    Task 3: Add Django REST Framework to project settings

--- cyberpolygon/backend/task2/nelexeyProject/rts/settings.py
+++ cyberpolygon/backend/task2/nelexeyProject/rts/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'users',
     'tests',
     'tester_panel',
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -140,3 +142,15 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.User'
+
+# REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
